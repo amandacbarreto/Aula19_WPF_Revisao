60727b2efddc50f68c35206a0cb92b84941ea381
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-06-30 16:42:32 -0300

    Acrescenta resolução da mesa de trabalho

---
+++ App/AppClient/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using AppClient.ViewModels;
+using System.Windows;
+
+namespace AppClient
+{
+    public class Bootstrapper: BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            // Inicializa o projeto
+            base.Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Define qual será o Controlador padrão
+            // Nota: O controlador MainViewModel ainda não foi criado nessa etapa.
+            base.DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
